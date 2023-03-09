076fa81df10f1adedc43168d0c12b28d9c1bd445
jeffvilla <jvilla125@gmail.com>
2023-03-09 15:02:06 -0800

    User can now click the 'Start Game' button and the div with id='intro-page' will disappear

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 // Cached 'intro-page' div after clicking start 
 const introPage = document.getElementById("intro-page");
+const startButton = document.getElementById("start-button");
+
+// event listeners
+startButton.addEventListener('click', init);
+
+// functions
+function init(){
+    introPage.style.display = "none"
+}
