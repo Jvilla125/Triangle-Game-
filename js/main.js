f90c99698f82967255616896129d21210e2b2b51
jeffvilla <jvilla125@gmail.com>
2023-03-09 15:08:12 -0800

    Add two buttons 'refresh home' and 'restart game' button to appear after init() function is called

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // Cached 'intro-page' div after clicking start 
 const introPage = document.getElementById("intro-page");
 const startButton = document.getElementById("start-button");
-
+const gamePage = document.getElementById("game-page");
 // event listeners
 startButton.addEventListener('click', init);
 
 // functions
 function init(){
     introPage.style.display = "none"
+    gamePage.style.display = "flex"
+
 }
