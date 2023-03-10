60240c6d38d3e4999462fd1afb8754b2979bebff
jeffvilla <jvilla125@gmail.com>
2023-03-10 13:46:45 -0800

    Update the css to include a board game after user clicks on start game button

--- js/main.js
+++ js/main.js
@@ -5,9 +5,33 @@ const gamePage = document.getElementById("game-page");
 // event listeners
 startButton.addEventListener('click', init);
 
+// constants
+let rows = 5
+let columns = 9;
+let currentColumns = [];
 // functions
 function init(){
     introPage.style.display = "none"
     gamePage.style.display = "flex"
+    rendor();
+}
 
+function rendor(){
+    setGame();
+}
+
+function setGame(){
+    board = [];
+    currentColumns = [5, 5, 5, 5, 5, 5, 5, 5, 5];
+    for (let r = 0; r < rows; r++){
+        let row = [];
+        for (let c = 0; c < columns; c++){
+            row.push(' ');
+            let chip = document.createElement('div');
+            chip.id = r.toString() + "-" + c.toString();
+            chip.classList.add("chip");
+            
+        }
+        board.push(row)
+    }
 }
